d5c3df238d03c480f000e29d70270212c2af8774
Zyan <coderZyan@outlook.com>
2017-04-01 13:46:49 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-// // 基于准备好的dom，初始化echarts实例
-// var myChart = echarts.init(document.getElementById('main'));
-
-// // 指定图表的配置项和数据
-// var option;
-
-// var categories = ['项目前期', '项目中期', '项目后期'];
-
-// option = {
-//   legend: [{
-//     // selectedMode: 'single',
-//     data: categories.map(function (a) {
-//       return a.name;
-//     })
-//   }],
-//   series: [{
-//     name: '前端技术构成',
-//     type: 'graph',
-//     legendHoverLink: true,
-//     hoverAnimation: true,
-//     layout: 'force',
-//     data: []
-//   }]
-// };
-
-// 使用刚指定的配置项和数据显示图表。
-// myChart.setOption(option);
-
-
 //资产配置数据图表
 var moneyChart = echarts.init(document.querySelector('#money'));
 var money = {
