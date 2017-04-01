23e5acd2bb4353a9b45112200d74abe3c4cfba9e
Zyan <coderZyan@outlook.com>
2017-04-01 13:40:26 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main'));
+// // 基于准备好的dom，初始化echarts实例
+// var myChart = echarts.init(document.getElementById('main'));
 
-// 指定图表的配置项和数据
-var option;
+// // 指定图表的配置项和数据
+// var option;
 
-var categories = ['项目前期', '项目中期', '项目后期'];
+// var categories = ['项目前期', '项目中期', '项目后期'];
 
-option = {
-  legend: [{
-    // selectedMode: 'single',
-    data: categories.map(function (a) {
-      return a.name;
-    })
-  }],
-  series: [{
-    name: '前端技术构成',
-    type: 'graph',
-    legendHoverLink: true,
-    hoverAnimation: true,
-    layout: 'force',
-    data: []
-  }]
-};
+// option = {
+//   legend: [{
+//     // selectedMode: 'single',
+//     data: categories.map(function (a) {
+//       return a.name;
+//     })
+//   }],
+//   series: [{
+//     name: '前端技术构成',
+//     type: 'graph',
+//     legendHoverLink: true,
+//     hoverAnimation: true,
+//     layout: 'force',
+//     data: []
+//   }]
+// };
 
 // 使用刚指定的配置项和数据显示图表。
 // myChart.setOption(option);
